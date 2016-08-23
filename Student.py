726c9e61e5413004b6a4bd53cebddb9d3431c251
RaghavaDhanya <raghava.dhanya@gmail.com>
2016-08-23 18:56:06 +0530

    Using OOPs

---
+++ Student.py
@@ -0,0 +1,11 @@
+__author__ = 'Raghava'
+
+class Students:
+    def __init__(self,name,sem,result,marks,total):
+        self.name=name
+        self.sem=sem
+        self.result=result
+        self.marks=marks
+        self.total=total
+    def __str__(self):
+        return self.name+"_"+str(self.marks)+"_"+str(self.sem)+"_"+str(self.result)
--- main.py
+++ main.py
@@ -2,30 +2,38 @@ __author__ = 'Raghava'
 import urllib2
 import tempfile
 import linecache
-
+import Student
 from bs4 import BeautifulSoup
-usn="1mv14cs"
-names=[]
-sub=[[],[],[],[],[],[],[],[]]
-for i in range(1,140):
-    usn1=usn+(3-len(str(i)))*"0"+str(i)
-    htmldata= urllib2.urlopen(r'http://results.vtu.ac.in/?submit=1&rid={}'.format(usn1)).read()
+
+usn = "1mv14cs"
+list = []
+for i in range(1, 10):
+    usn1 = usn + (3 - len(str(i))) * "0" + str(i)
+    htmldata = urllib2.urlopen(r'http://results.vtu.ac.in/?submit=1&rid={}'.format(usn1)).read()
     temp = tempfile.NamedTemporaryFile(delete=False)
     temp.flush()
     temp.write(htmldata)
-    soup = BeautifulSoup(linecache.getline(temp.name, int(242)),"lxml")
+    soup = BeautifulSoup(linecache.getline(temp.name, int(242)), "lxml")
     # print(soup.prettify())
 
     complete_text = soup.getText()
-    if (len(complete_text)>10):
-        tables= soup('table')
-        names+=[soup('b')[0].text]
-        k=0
-        for row in (tables[1].find_all('tr')[1:]) :
-            if(k<8):
+    if len(complete_text) > 10:
+        tables = soup('table')
+        name = soup('b')[0].text
+        marks = {}
+        sem = ''
+        result = ''
+        for tr in tables[0].find_all('tr'):
+            td = tr.find_all('td')
+            sem = td[1].text
+            result = "".join([i if ord(i) < 128 else ' ' for i in td[3].text])
+        k = 0
+        for row in (tables[1].find_all('tr')[1:]):
+            if k < 8:
                 tds = row.find_all('td')
-                sub[k]+=[tds[3].text]
-                k+=1
+                marks[tds[0].text] = (tds[1].text, tds[2].text, tds[3].text, tds[4].text)
+                k += 1
+        list += [Student.Students(name, sem, result, marks, 100)]
     temp.close()
-for i in range(0,len(names)) :
-    print(names[i],sub[0][i],sub[1][i],sub[2][i],sub[3][i],sub[4][i],sub[5][i],sub[6][i],sub[7][i])
+for i in range(0, len(list)):
+    print(str(list[i]))
