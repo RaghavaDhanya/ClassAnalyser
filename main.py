3c477c9bb7cd17cb5b328389222249d210dc3761
RaghavaDhanya <raghava.dhanya@gmail.com>
2016-08-27 18:49:56 +0530

    Fixing the things which broke after new results

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import linecache
 import Student
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
-usn = "1mv14ec"
+usn = "1mv14cs"
 list = []
 for i in range(1, 50):
     usn1 = usn + (3 - len(str(i))) * "0" + str(i)
@@ -23,20 +23,26 @@ for i in range(1, 50):
         marks = {}
         sem = ''
         result = ''
-        total=0
+        #total=0
         for tr in tables[0].find_all('tr'):
             td = tr.find_all('td')
             sem = td[1].text
             result = "".join([i if ord(i) < 128 else ' ' for i in td[3].text])
         k = 0
         for row in (tables[1].find_all('tr')[1:]):
-            if k < 8:
-                tds = row.find_all('td')
-                marks[tds[0].text] = (tds[1].text, tds[2].text, tds[3].text, tds[4].text)
-                k += 1
-        total="".join([i if ord(i) < 128 else ' ' for i in tables[2].find_all('tr')[0].find_all('td')[3].text])
-        list += [Student.Students(name, sem, result, marks, total)]
+            tds = row.find_all('td')
+            try:
+                if tds[0].text!='Subject':
+                    marks[tds[0].text] = (tds[1].text, tds[2].text, tds[3].text, tds[4].text)
+            except IndexError:
+                pass
+            k += 1
+        #Total doesn't exist for all results
+        #total="".join([i if ord(i) < 128 else ' ' for i in tables[2].find_all('tr')[0].find_all('td')[3].text])
+        list += [Student.Students(name, sem, result, marks)]
     temp.close()
+# for i in list:
+#     print(i)
 arr={}
 for i in list:
     for j in i.marks.keys():
